a5ecab7042a056d1055c02e83c8980748b9d2e12
Gustavo Freitas Alves <58485863+GustavoFA@users.noreply.github.com>
2022-11-09 10:46:15 -0300

    Update BME680_OLED.py

--- BME680_OLED.py
+++ BME680_OLED.py
@@ -11,73 +11,49 @@ import ssd1306
 i2c = I2C(0, scl=Pin(21), sda=Pin(20))
 
 # Inicializando e configurando a I2C dos dispositivos
-bme = BME680_I2C(i2c=i2c)
-display = ssd1306.SSD1306_I2C(128, 32, i2c)
+bme = BME680_I2C(i2c=i2c)					# End: 119
+display = ssd1306.SSD1306_I2C(128, 32, i2c)	# End: 60
 
 # Configuração das GPIOs para Botão e Leds
-button = Pin(28, Pin.IN, Pin.PULL_UP)
-#led = Pin(25, Pin.OUT)
-led_R = Pin(19, Pin.OUT)
-led_G = Pin(18, Pin.OUT)
-led_B = Pin(17, Pin.OUT)
+button = Pin(22, Pin.IN, Pin.PULL_UP)
+led = Pin(25, machine.Pin.OUT)
+led_B = Pin(19, machine.Pin.OUT)
+led_G = Pin(18, machine.Pin.OUT)
+led_R = Pin(27, machine.Pin.OUT) 
 
-#led.value(0)
-# turning off led rgb
-led_R.value(1)
+led.value(0)	# não está sendo usada
+led_R.value(0)
 led_G.value(0)
 led_B.value(1)
 
 # Variáveis
-state = -1
+state = 0
 disp_prob = 0
 relogio_oled = ticks_ms()
 relogio_bme = ticks_ms()
-relogio_scan = ticks_ms()
+relogio_scan = ticks_ms() 
 relogio_led = ticks_ms()
 
-def blink_alert():
-    led_R.value(1)
-    sleep_ms(400)
-    led_R.toggle()
+# Tempo para os dispositivos iniciarem
+sleep_ms(1500)
 
-def blink_functional():
-    led_G.value(1)
-    sleep_ms(200)
-    led_G.toggle()
 
-def check_device_alert(device_id):
-    if device_id == 60:
-        blink_alert()
-#     elif end == 119:
-#         led_R.off()
-#         led_G.on()
+def RGB_f(end):
+    if end == 60:     
+        led_R.on()
+        led_G.off()
+    elif end == 119:
+        led_R.off()
+        led_G.on()
     else:
-        led_R.value(1)
-        led_G.value(1)
-        led_B.value(1)
+        led_R.off()
+        led_G.off()
 
-def set_error_state():
-    led_G.value(1)
-    led_R.value(0)
-
-def set_normal_state():
-    led_G.value(0)
-    led_R.value(1)
 
 # Laço principal
-while True:
-    """
-    LED em verde indica que o circuito está funcional
-    LED pisca em verde quando executa leitura do sensor
-
-    """
-    set_normal_state()
-
-    # Verificação dos dispositivos conectados a cada 10 s
-    if(ticks_ms() - relogio_scan > 10000):
-        relogio_scan = ticks_ms()
-
-
+while True:          
+        
+    
     # Leitura do botão com debounced
     if(button.value() == 0):
         sleep_ms(150)	# Debounced
@@ -85,81 +61,105 @@ while True:
             state = state + 1
             if state > 4:
                 state = 0
-            blink_functional()
-            print(f'button state: {state}')
-
+            #print(state)            
+        
+    
     ''' Tentativa de impressão no OLED, se caso o mesmo não estiver conectado
     não será possível entrar no if seguinte.
     '''
+    
     try:
-        diff = ticks_ms() - relogio_bme
-        print(diff)
+        
         # Obtenção dos parâmetros a cada 500 ms
-        if(diff > 10000):
-            print('reading data')
-            blink_functional()
-            f_temperature = "%0.1f C" % bme.temperature
-            f_humidity = "%0.1f %%" % bme.humidity
-            f_pressure = "%0.3f hPa" % bme.pressure
+        if(ticks_ms() - relogio_bme > 500):
             temp = str(round(bme.temperature, 2)) + ' C'
-            print(f'\nTemperatura: {f_temperature}')
             hum = str(round(bme.humidity, 2)) + ' %'
-            print(f'Umidade: {f_humidity}')
             pres = str(round(bme.pressure, 2)) + ' hPa'
-            print(f'Pressão: {f_pressure}\n')
             ar = str(round(bme.gas/1000, 2)) + ' KOhms'
             alt = str(round(bme.altitude, 2)) + ' m'
             relogio_bme = ticks_ms()
-
-#         if (ticks_ms() - relogio_oled) > 500:
-#         relogio_oled = ticks_ms()
-        if(state == 0):
-            display.fill(0)
-            display.text('Pressao: ', 0, 0)
-            display.text(pres, 0, 24)
-            display.show()
-        elif(state == 1):
-            display.fill(0)
-            display.text('Temperatura: ', 0, 0)
-            display.text(temp, 0, 24)
-            display.show()
-        elif(state == 2):
-            display.fill(0)
-            display.text('Umidade Relativa: ', 0, 0)
-            display.text(hum, 0, 24)
-            display.show()
-        elif(state == 3):
-            display.fill(0)
-            display.text('Altitude: ', 0, 0)
-            display.text(alt, 0, 24)
-            display.show()
-        elif(state == 4):
-            display.fill(0)
-            display.text('Qualidade do Ar: ', 0, 0)
-            display.text(ar, 0, 24)
-            display.show()
-        disp_prob = 0
-        if disp_prob != 0:
-            check_device_alert(0)
-        led_G.value(0)
-        sleep_ms(500)
-    except OSError as ose:
-        set_error_state()
-        print(f'OSError: {ose}')
+    
+    
+        if (ticks_ms() - relogio_oled) > 500:
+            
+            relogio_oled = ticks_ms()
+        
+            if(state == 0):
+                display.fill(0)
+                display.text('Temperatura: ', 0, 0)
+                display.text(temp, 0, 24)
+                display.show()
+            elif(state == 1):
+                display.fill(0)
+                display.text('Umidade Relativa: ', 0, 0)
+                display.text(hum, 0, 24)
+                display.show()
+            elif(state == 2):
+                display.fill(0)
+                display.text('Pressao: ', 0, 0)
+                display.text(pres, 0, 24)
+                display.show()
+            elif(state == 3):
+                display.fill(0)
+                display.text('Altitude: ', 0, 0)
+                display.text(alt, 0, 24)
+                display.show()
+            elif(state == 4):
+                display.fill(0)
+                display.text('Qualidade do Ar: ', 0, 0)
+                display.text(ar, 0, 24)
+                display.show()
+                
+            #if disp_prob != 0:
+                #print('aqui')
+                
+            disp_prob = 0
+            led_B(1)
+            led_G(0)
+            led_R(0)
+            
+    except:
+        
+    
+        
         if(disp_prob == 0):
             devices = i2c.scan()
-            print(f'Devices found: {devices}')
-            if(devices[0] != 60):
-                check_device_alert(60)
+            
+            if(devices[0] != 119):
+                RGB_f(119)
+                led_B(0)
+                disp_prob = 2 
+            elif(devices[0] != 60):
+                RGB_f(60)
+                led_B(0)
                 disp_prob = 1
+        elif(disp_prob == 2):
+            if(ticks_ms() - relogio_led > 500):
+                led_R.toggle()
+                relogio_led = ticks_ms()
+                '''
+                devices = i2c.scan()
+                if devices > 1:
+                    if devices[1] == 119:
+                        display = ssd1306.SSD1306_I2C(128, 32, i2c)	# End: 119
+                elif devices > 0:
+                    if devices[0] == 119:
+                        display = ssd1306.SSD1306_I2C(128, 32, i2c)	# End: 119
+                '''                    
         elif(disp_prob == 1):
             if(ticks_ms() - relogio_led > 500):
-                check_device_alert(60)
+                led_G.toggle()
                 relogio_led = ticks_ms()
-        sleep_ms(5000)
-    except BaseException as e:
-        print(type(e))
-        print(f'Unexpected {e}')
-
-
+                '''
+                devices = i2c.scan()
+                if devices > 1:
+                    if devices[1] == 60:
+                        bme = BME680_I2C(i2c=i2c)					# End: 60
+                elif devices > 0:
+                    if devices[0] == 60:
+                        bme = BME680_I2C(i2c=i2c)					# End: 60
+                '''
+        
+        
+        
 
