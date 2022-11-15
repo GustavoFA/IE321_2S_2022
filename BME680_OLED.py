f01d0641d224c3e622f098a111241d4178f69a2e
Gustavo Freitas Alves <58485863+GustavoFA@users.noreply.github.com>
2022-11-15 17:02:25 -0300

    Update BME680_OLED.py
    Este código é uma junção dos códigos da branch do Manassés e do Gustavo. Segui o laço principal feito por mim e algumas definições iniciais, como tipos de sinalização, códigos de erro, frequência de oscilação seguir do Manassés.

--- BME680_OLED.py
+++ BME680_OLED.py
@@ -1,10 +1,31 @@
-# Código para validação do funcionamento do BME680 + OLED
+"""
+Código para validação do funcionamento do BME680 + OLED
+Botão de chaveamento de informações do sensor
+    O botão serve para chavear no display as diferentes grandezas capturadas pelo sensor:
+        * Pressão Atmosférica
+        * Temperatura
+        * Umidade
+        * Qualidade do ar
+        * Altitude
+LED RGB de indicação de funcionamento ou falha
+    * LED em verde indica que o circuito está funcional
+    * LED pisca em verde quando executa leitura do sensor
+    * LED pisca em verde quando pressiona o botão
+    * LED pisca em vermelho quando há falha de leitura do sensor
+    * LED pisca em azul quando há falha de leitura do OLED
+"""
 
 # Bibliotecas
 from machine import Pin, I2C
 from bme680 import *
 from time import ticks_ms, sleep_ms
 import ssd1306
+
+# Definição dos valores de on/off para LED RGB:
+# Anodo comum  => on=0, off=1
+# Catodo comum => on=1, off=0
+LED_ON = 1
+LED_OFF = 0
 
 # Configuração I2C da Pico
 # Usaremos I2C 0, GPIO 20 e 21 --> SDA e SCL
@@ -22,9 +43,9 @@ led_G = Pin(18, machine.Pin.OUT)
 led_R = Pin(27, machine.Pin.OUT) 
 
 led.value(0)	# não está sendo usada
-led_R.value(0)
-led_G.value(0)
-led_B.value(1)
+led_R.value(LED_OFF)
+led_G.value(LED_ON)
+led_B.value(LED_OFF)
 
 # Variáveis
 state = 0
@@ -33,21 +54,28 @@ relogio_oled = ticks_ms()
 relogio_bme = ticks_ms()
 relogio_scan = ticks_ms() 
 relogio_led = ticks_ms()
+button_time = ticks_ms()
+
+# Tempo de cada conjunto de medidas (ms)
+tempo_par = 500
 
 # Tempo para os dispositivos iniciarem
-sleep_ms(1500)
+sleep_ms(2000)
 
 
+# Função para definir iluminação para cada situação do sistema
 def RGB_f(end):
     if end == 60:     
-        led_R.on()
-        led_G.off()
+        led_R.value(LED_OFF)
+        led_G.value(LED_OFF)
+        led_B.value(LED_ON)
     elif end == 119:
-        led_R.off()
-        led_G.on()
+        led_R.value(LED_ON)
+        led_G.value(LED_OFF)
+        led_B.value(LED_OFF)
     else:
-        led_R.off()
-        led_G.off()
+        led_R.value(LED_OFF)
+        led_B.value(LED_OFF)
 
 
 # Laço principal
@@ -56,31 +84,38 @@ while True:
     
     # Leitura do botão com debounced
     if(button.value() == 0):
-        sleep_ms(150)	# Debounced
-        if(button.value() == 0):
+        # Debounced
+        if(button.value() == 0) and (ticks_ms() - button_time > 150):
+            button_time = ticks_ms()
             state = state + 1
             if state > 4:
-                state = 0
-            #print(state)            
+                state = 0           
         
-    
-    ''' Tentativa de impressão no OLED, se caso o mesmo não estiver conectado
-    não será possível entrar no if seguinte.
-    '''
     
     try:
         
+        if(disp_prob == 0):
+            if(ticks_ms() - relogio_bme < 250):
+                led_G.value(LED_ON)
+                led_G(LED_OFF)
+                led_R(LED_OFF)
+            else:
+                led_G.value(LED_OFF)
+                led_G(LED_OFF)
+                led_R(LED_OFF)
+        
         # Obtenção dos parâmetros a cada 500 ms
-        if(ticks_ms() - relogio_bme > 500):
+        if(ticks_ms() - relogio_bme > tempo_par):
             temp = str(round(bme.temperature, 2)) + ' C'
             hum = str(round(bme.humidity, 2)) + ' %'
             pres = str(round(bme.pressure, 2)) + ' hPa'
             ar = str(round(bme.gas/1000, 2)) + ' KOhms'
             alt = str(round(bme.altitude, 2)) + ' m'
             relogio_bme = ticks_ms()
+            disp_prob = 0
     
     
-        if (ticks_ms() - relogio_oled) > 500:
+        if (ticks_ms() - relogio_oled) > 2*tempo_par:
             
             relogio_oled = ticks_ms()
         
@@ -109,57 +144,33 @@ while True:
                 display.text('Qualidade do Ar: ', 0, 0)
                 display.text(ar, 0, 24)
                 display.show()
-                
-            #if disp_prob != 0:
-                #print('aqui')
-                
             disp_prob = 0
-            led_B(1)
-            led_G(0)
-            led_R(0)
             
-    except:
-        
-    
+    except OSError as ose:
         
         if(disp_prob == 0):
             devices = i2c.scan()
             
             if(devices[0] != 119):
                 RGB_f(119)
-                led_B(0)
                 disp_prob = 2 
             elif(devices[0] != 60):
                 RGB_f(60)
-                led_B(0)
                 disp_prob = 1
         elif(disp_prob == 2):
             if(ticks_ms() - relogio_led > 500):
                 led_R.toggle()
-                relogio_led = ticks_ms()
-                '''
-                devices = i2c.scan()
-                if devices > 1:
-                    if devices[1] == 119:
-                        display = ssd1306.SSD1306_I2C(128, 32, i2c)	# End: 119
-                elif devices > 0:
-                    if devices[0] == 119:
-                        display = ssd1306.SSD1306_I2C(128, 32, i2c)	# End: 119
-                '''                    
+                relogio_led = ticks_ms()                   
         elif(disp_prob == 1):
             if(ticks_ms() - relogio_led > 500):
-                led_G.toggle()
+                led_B.toggle()
                 relogio_led = ticks_ms()
-                '''
-                devices = i2c.scan()
-                if devices > 1:
-                    if devices[1] == 60:
-                        bme = BME680_I2C(i2c=i2c)					# End: 60
-                elif devices > 0:
-                    if devices[0] == 60:
-                        bme = BME680_I2C(i2c=i2c)					# End: 60
-                '''
         
+    except KeyboardInterrupt as k:
+        sys.exit()
+    except BaseException as e:
+        print(type(e))
+        print(f'Unexpected {e}')
         
         
 
